bdf7ae8022fbba1f0631fe5282ec308baab815a5
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-03-13 00:53:46 +0100

    ft(components)
    - add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 // react libraries
 import React from "react";
 
+// component
+import Header from "./components/Header";
+
 /**
  * @desc app entry point
  */
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+// react libraries
+import React from "react";
+
+/**
+ * @desc header component
+ */
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <span>Gmail</span>
+        <span>Images</span>
+        <span>SignIn</span>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
